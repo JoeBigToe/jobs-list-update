6e3b22ec22f0adb63aec32d82eac9ae4b028cabb
Joao Rosa <joao.rosa@infor.com>
2018-01-19 14:20:36 +0100

    First commit

---
+++ httpTrigger/dev_mock.js
@@ -0,0 +1,43 @@
+var AzureFunction = require('./index');
+
+// Local development query and body params
+var debugQuery = {
+    "code": "This is the code"
+}
+var debugBody = {
+    "name": "Mock"
+}
+// Local development request object
+var req = {
+    originalUrl: 'http://original-azure-function-url',
+    method: 'GET',
+    query: debugQuery,
+    headers: { 
+        connection: 'Keep-Alive',
+        accept: 'application/json',
+        host: 'original-azure-function-url',
+        origin: 'https://functions.azure.com',
+    },
+    body: debugBody,
+    rawBody: JSON.stringify(debugBody)
+};
+// Local development context
+var debugContext = {
+    invocationId: 'ID',
+    bindings: {
+        req
+    },
+    log: function () {
+        var util = require('util');
+        var val = util.format.apply(null, arguments);
+        console.log(val);
+    },
+    done: function () {
+        // When done is called, it will log the response to the console
+        console.log('Response:', this.res);
+    },
+    res: null
+};
+
+// Call the AzureFunction locally with your testing params
+AzureFunction(debugContext, req);
---
+++ timeTrigger/index.js
@@ -0,0 +1,229 @@
+var request = require("request")
+var mg = require('mongodb').MongoClient;
+
+var postingsDb;
+var closedDb;
+var dbHandler;
+
+
+// Uri's for Azure Cosmos DB
+var user = "jobs-list";
+var pass = "z2We6g4uL66Z4ccr2IyQ4AYxXPhnUWLuOqEDJQBdU1GumN5WefU221U8BxADcWWhVv8iyiaejzUZNVlYlMTg9g==";
+var mgdburl = "mongodb://jobs-list.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+
+// Uri's for nofluffjobs
+var nfjall = "https://nofluffjobs.com/api/posting?criteria=";
+var nfjone = "https://nofluffjobs.com/api/postingNew/";
+
+// Global flags used to synchronize code
+var newJobsReady = false;
+var closedJobsReady = false;
+var storedJobsReady = false;
+var onlineJobsReady = false;
+var allNewJobsInserted = false;
+var allClosedJobsInserted = false;
+var allClosedJobsRemoved = false;
+
+var onlineJobs = {};
+var storedJobs = {};
+var newJobs = {};
+var closedJobs = {};
+
+var close = function (context) {
+
+    if(!allNewJobsInserted || !allClosedJobsInserted || !allClosedJobsRemoved){
+        setTimeout(
+            () => {close(context);},
+            1000
+        );
+    } else {
+        dbHandler.close();
+        context.log("Connection to db closed!");
+        context.done();
+    }
+
+}
+
+var processOpenAndClosedJobs = function (context){
+
+    if(!newJobsReady || !closedJobsReady){
+        setTimeout(
+            () => {processOpenAndClosedJobs(context);},
+            500
+        );
+    } else {
+    
+        if (newJobs.length === 0) {allNewJobsInserted=true;};
+        if (closedJobs.length === 0) {allClosedJobsRemoved=true;allClosedJobsInserted=true;};
+
+        // Insert new jobs into collection 'postings'
+        newJobs.forEach(job => {
+            // Fecth detailed information about this job
+            request(
+                {
+                    url: nfjone+job.id,
+                    json: true
+                },
+                function(err, body, response){
+                    if (err) throw err;
+
+                    // Store it in mongoDB                    
+                    postingsDb.insertOne(
+                        response.posting,
+                        function (err, res) {
+                            if (err) throw err;
+
+                            if ((newJobs.length-1) === newJobs.indexOf(job)) {allNewJobsInserted=true;}
+                            context.log(`Job inserted: ${response.posting.id}`);
+                        }
+                    );
+
+                }
+            );
+
+        });
+        
+        // Process closed jobs
+        // 1. Copy this job element from postings collection to closed
+        // 2. Delete this job element from postings
+        closedJobs.forEach(job => {
+            // Remove closed jobs from collection 'postings'
+            postingsDb.deleteOne(
+                {id: job.id},
+                function(err, obj){
+                    if (err) throw err;
+
+                    if((closedJobs.length-1) === closedJobs.indexOf(job)) {allClosedJobsRemoved=true;}
+                    context.log(`Job removed from postings: ${job.id}`);
+                }
+            );
+
+            // Insert closed jobs into collection 'closed'
+            job.closed = new Date().toDateString();
+            closedDb.insertOne(
+                job,
+                function(err, obj){
+                    if (err) throw err;
+
+                    if((closedJobs.length-1) === closedJobs.indexOf(job)) {allClosedJobsInserted=true;}
+                    context.log(`Job inserted into deleted: ${job.id}`);
+                }
+
+            );
+        });
+
+    }
+
+    // Async function which will check if all tasks are completed
+    // Once completed, it will close db connection and terminate function
+    close(context);
+
+
+}
+
+// Work with job data
+// This function will wait until onlineJobs and storedJobs data is available
+// storedJobs is an array with just the job Id 
+// onlineJobs has limited data
+var workWithJobData = function (context){
+
+    if(!onlineJobsReady || !storedJobsReady){
+        setTimeout(
+            () => {workWithJobData(context);},
+            1000
+        );
+    } else { 
+        
+        // Get the new jobs wich are not stored yet
+        newJobs = onlineJobs.filter(
+            function(online){
+                if (onlineJobs.indexOf(online) === (onlineJobs.length-1)) {
+                    newJobsReady=true;
+                }
+                return !storedJobs.find(
+                    function(stored){
+                        return online.id === stored.id;
+                    }
+                );
+            }
+        );
+    
+        // Get the closed jobs
+        // Present in database but not in the online results
+        closedJobs = storedJobs.filter(
+            function(stored){
+                if (storedJobs.indexOf(stored) === (storedJobs.length - 1)) {
+                    closedJobsReady=true;
+                }
+                return !onlineJobs.find(
+                    function(online){
+                        return online.id === stored.id;
+                    }
+                );
+            }
+        );
+
+        processOpenAndClosedJobs(context);
+    }
+    
+}
+
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    
+    // Gets Ids of currently active jobs
+    request(
+        {
+            url: nfjall,
+            json: true
+        }, 
+        function(error, body, response){
+            if(error) throw error;
+
+            onlineJobs = response.postings;
+            onlineJobsReady = true;
+
+        }
+    ); 
+    
+    // Async function that takes all stored jobs IDs
+    // Once the function is ready, the flag storedJobsReady turns to True
+    mg.connect(
+        mgdburl,
+        {auth: {
+            user: user,
+            password: pass,
+        }},
+        function (err, db){
+            if (err) throw err;
+            
+            // Search for new found jobs and insert
+            dbHandler = db;
+            postingsDb = db.db("jobs").collection("postings");
+            closedDb = db.db("jobs").collection("closed");
+            
+            postingsDb.find({}).toArray(
+                function(err, result){
+                    if (err) throw err;
+
+                    storedJobs = result;
+                    storedJobsReady = true;
+                    //db.close();
+                }
+            );
+            
+        }
+    );
+
+    // Work with job data
+    // This function will wait until onlineJobs and storedJobs data is available
+    // storedJobs is an array with just the job Id 
+    // onlineJobs has limited data
+    workWithJobData(context);
+    
+};
